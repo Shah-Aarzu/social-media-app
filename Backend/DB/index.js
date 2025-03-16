6fc5265c5c3b5317f825481fc81290ec2c2ad05a
Shah-Aarzu <aarzumustafa4@gmail.com>
2025-03-16 14:15:33 +0530

    First-Commit

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL,{
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
