c9d223547ed9867eb02dbfe41bd1f96862716d05
Shah-Aarzu <aarzumustafa4@gmail.com>
2025-03-16 15:21:29 +0530

    First-Commit

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error.message);
+    console.error(`MongoDB Not Connected : ${error.message}`);
 
     process.exit(1);
   }
