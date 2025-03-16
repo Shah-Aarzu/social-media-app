7915b1bf74e87b683255c0e4a56b46a47eaeae7c
Shah-Aarzu <aarzumustafa4@gmail.com>
2025-03-16 16:52:08 +0530

    First-Commit

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
      console.log(`MONGODB_URL: ${process.env.MONGODB_URL}`);
     console.log(`EMAIL : ${process.env.EMAIL}`);
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect("mongodb+srv://new_user_31:aarzu6126@social-media-app-db.jd9cw.mongodb.net/?retryWrites=true&w=majority&appName=social-media-app-db");
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
