93863524f10031571835609b9cd1bfcadab7e394
Shah-Aarzu <aarzumustafa4@gmail.com>
2025-04-13 12:34:23 +0530

    Update index.js

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://new_user_31:aarzu6126@social-media-app-db.jd9cw.mongodb.net/?retryWrites=true&w=majority&appName=social-media-app-db");
+    const conn = await mongoose.connect("mongodb url");
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
